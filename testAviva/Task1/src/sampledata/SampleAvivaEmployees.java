a598aa4ef909ff6b1f3beef624cca03976a02e12
Anastasiia-Khab <nastuha97@gmail.com>
2017-04-09 23:52:07 +0300

    second commit

--- testAviva/Task1/src/sampledata/SampleAvivaEmployees.java
+++ testAviva/Task1/src/sampledata/SampleAvivaEmployees.java
@@ -7,7 +7,7 @@ import src.model.Employee;
 import com.google.common.collect.ImmutableList;
 
 public class SampleAvivaEmployees {
-
+	// second commit
 	public static final Employee JOHN = new Employee("JOHN");
 	public static final Employee ANNA = new Employee("ANNA");
 	public static final Employee ROB = new Employee("ROB");
